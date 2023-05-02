91d0ace473ef97aeebb1c2afd7fe98f0953667f5
Fef97 <federicakr97@gmail.com>
2023-05-02 20:19:16 +0200

    finishhh

--- JS/main.js
+++ JS/main.js
@@ -1,37 +1,44 @@
+let quadrato
+
 // selezionare un elemento contenitore
 const contenitore = document.getElementById("contenitore");
 console.log(contenitore);
+
 
 
 // cicliamo le operazioni di creazione e di output
 for (let i = 1; i <= 100; i++) {
     // console.log(i);
     
+    
     // aggiungiamo condizione
-    // prima condizione, se è divisibie per 3 allora FIZZ
-    if (i % 3 == 0) {
-    // creiamo un nuovo elemeto
-    let quadrato = `<span class="square">${"fizz"}</span>`;
-    console.log(quadrato);
-     
-    // se è divisibile per 5 allora buzz
+    // prima condizione, se è divisibie per 5 e per 3 allora FIZZbuzz
+    if (i % 3 == 0 && i % 5 == 0)  {
+        // creiamo un nuovo elemeto
+        quadrato = `<span class="square fizzBuzz">${"fizzBuzz"}</span>`;
+        console.log(quadrato);
+        
+        // se è divisibile per 5 allora buzz
     } else if (i % 5 == 0) {
-        let quadrato = `<span class="square">${"buzz"}</span>`;
+        quadrato = `<span class="square buzz">${"buzz"}</span>`;
+        
         console.log(quadrato);
     
-    } else if () {
-        let quadrato = `<span class="square">${i}</span>`;
+        // se è divisibile per 3 allora fizz
+    } else if (i % 3 == 0) {
+        
+        quadrato = `<span class="square fizz">${"fizz"}</span>`;
         console.log(quadrato);
     
     
-    }else {
-        let quadrato = `<span class="square">${"fizzBuzz"}</span>`;
+    } else {
+        quadrato = `<span class="square normal">${i}</span>`;
         console.log(quadrato);
     }
     
+    contenitore.innerHTML += quadrato;
     
 }
 
-
-
 // aggiungiamo al contenitore l'elemento quadrato
+
